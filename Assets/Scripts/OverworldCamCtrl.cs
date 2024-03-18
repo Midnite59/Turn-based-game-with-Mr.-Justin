d55aed3af89b95635f0eb55bf70da39b931147d7
Midnite59 <awsomekai789@gmail.com>
2024-03-18 17:02:32 -0400

    Made a Camera Yay!! 3/18/24
    Yay!!1

--- Assets/Scripts/OverworldCamCtrl.cs
+++ Assets/Scripts/OverworldCamCtrl.cs
@@ -10,6 +10,10 @@ public class OverworldCamCtrl : MonoBehaviour
     public float CamDistance;
     public float CamDesiredDistance;
     public Vector3 CamDesiredDirection;
+    private float CamYaw;
+    private float CamPitch;
+    public float CamYawMultiplier;
+    public float CamPitchMultiplier;
 
 
     // Start is called before the first frame update
@@ -20,11 +24,29 @@ public class OverworldCamCtrl : MonoBehaviour
         CamDesiredDirection = (Camera.transform.position - CameraTarget.position).normalized;
         Camera.transform.position = CameraTarget.position + CamDesiredDirection * CamDesiredDistance;
         Camera.transform.LookAt(CameraTarget.position);
+        Vector3 CamEuler = Camera.transform.rotation.eulerAngles;
+        CamPitch = CamEuler.x;
+        CamYaw = CamEuler.y;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        
+        float InputY = Input.GetAxis("Mouse X");
+        float InputX = -Input.GetAxis("Mouse Y");
+        CamYaw += InputY * CamYawMultiplier;
+        CamPitch = Mathf.Clamp(CamPitch + InputX * CamPitchMultiplier, -89, 89);
+        if (CamYaw > 180)
+        {
+            CamYaw -= 360;
+        }
+        if (CamYaw < -180)
+        {
+            CamYaw += 360;
+        }
+        Quaternion CamNewEuler = Quaternion.Euler(CamPitch, CamYaw, 0);
+        CamDesiredDirection = CamNewEuler * -Vector3.forward;
+        Camera.transform.position = CameraTarget.position + CamDesiredDirection * CamDesiredDistance;
+        Camera.transform.LookAt(CameraTarget.position);
     }
 }
