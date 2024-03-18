22f61c2cbeb3c4d8196053220498d9a6d9f01feb
Midnite59 <awsomekai789@gmail.com>
2024-03-18 16:06:10 -0400

    Pushing some random scene changes
    3/18/2024

--- Assets/Scripts/OverworldCamCtrl.cs
+++ Assets/Scripts/OverworldCamCtrl.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class OverworldCamCtrl : MonoBehaviour
 {
+    public Camera Camera;
+    public Transform CameraTarget;
+    public Vector3 CamDirection;
+    public float CamDistance;
+    public float CamDesiredDistance;
+    public Vector3 CamDesiredDirection;
+
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState=CursorLockMode.Locked;
         Cursor.visible=false;
+        CamDesiredDirection = (Camera.transform.position - CameraTarget.position).normalized;
+        Camera.transform.position = CameraTarget.position + CamDesiredDirection * CamDesiredDistance;
+        Camera.transform.LookAt(CameraTarget.position);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
     }
