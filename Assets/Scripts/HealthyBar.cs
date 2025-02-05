f0e597679e92bfb428cd4b055e7922f4e147fa0d
Midnite59 <awsomekai789@gmail.com>
2025-02-05 17:12:27 -0500

    2/5/25 shippudden

--- Assets/Scripts/HealthyBar.cs
+++ Assets/Scripts/HealthyBar.cs
@@ -10,11 +10,12 @@ public class HealthyBar : MonoBehaviour
     public Image greenBar;
     public Image redBar;
     public Image blueBar;
-    public float redBarTime;
-    public float greenBarTime;
+    public float slowBarTime;
+    public float fastBarTime;
     public float bActorLastHP;
     public float redBarHP;
     public float greenBarHP;
+    public float blueBarHP;
     public float BarDelay;
 
     // Timers
@@ -26,6 +27,7 @@ public class HealthyBar : MonoBehaviour
 
     private Coroutine GreenBarRoutine = null;
     private Coroutine RedBarRoutine = null;
+    private Coroutine BlueBarRoutine = null;
 
     private bool slowBarLock;
     public void SetBattleActor(BattleActor battleActor)
@@ -39,13 +41,13 @@ public class HealthyBar : MonoBehaviour
 
     private IEnumerator FastBarFunctionDMG(float start)
     {
-        fastBarTimer = greenBarTime;
+        fastBarTimer = fastBarTime;
         float dif =  start - battleActor.hp;
         while (fastBarTimer > 0)
         {
             yield return null;
             fastBarTimer-=Time.deltaTime;
-            float t = fastBarTimer/greenBarTime;
+            float t = fastBarTimer/fastBarTime;
             t = fastBarCurve.Evaluate(t);
             greenBarHP = battleActor.hp+dif*t;
             greenBar.fillAmount = greenBarHP/battleActor.stats.Maxhp;
@@ -56,7 +58,7 @@ public class HealthyBar : MonoBehaviour
     }
     private IEnumerator SlowBarFunctionDMG(float start)
     {
-        slowBarTimer = redBarTime;
+        slowBarTimer = slowBarTime;
         float dif = start - battleActor.hp;
         while (slowBarLock)
         {
@@ -67,12 +69,53 @@ public class HealthyBar : MonoBehaviour
         {
             yield return null;
             slowBarTimer -= Time.deltaTime;
-            float t = slowBarTimer / redBarTime;
+            float t = slowBarTimer / slowBarTime;
             t = slowBarCurve.Evaluate(t);
             redBarHP = battleActor.hp + dif * t;
             redBar.fillAmount = redBarHP / battleActor.stats.Maxhp;
         }
         RedBarRoutine = null;
+        yield break;
+    }
+
+    private IEnumerator FastBarFunctionHeal(float start) 
+    {
+
+        fastBarTimer = fastBarTime;
+        float dif = start - battleActor.hp;
+        while (fastBarTimer > 0)
+        {
+            yield return null;
+            fastBarTimer -= Time.deltaTime;
+            float t = fastBarTimer / fastBarTime;
+            t = fastBarCurve.Evaluate(t);
+            blueBarHP = battleActor.hp + dif * t;
+            blueBar.fillAmount = blueBarHP / battleActor.stats.Maxhp;
+            redBar.fillAmount = blueBarHP / battleActor.stats.Maxhp;
+        }
+        BlueBarRoutine = null;
+        yield break;
+    }
+
+    private IEnumerator SlowBarFunctionHeal(float start)
+    {
+        slowBarTimer = slowBarTime;
+        float dif = start - battleActor.hp;
+        while (slowBarLock)
+        {
+            yield return null;
+        }
+        yield return new WaitForSeconds(BarDelay);
+        while (slowBarTimer > 0)
+        {
+            yield return null;
+            slowBarTimer -= Time.deltaTime;
+            float t = slowBarTimer / slowBarTime;
+            t = slowBarCurve.Evaluate(t);
+            greenBarHP = battleActor.hp + dif * t;
+            greenBar.fillAmount = greenBarHP / battleActor.stats.Maxhp;
+        }
+        GreenBarRoutine = null;
         yield break;
     }
 
@@ -111,6 +154,21 @@ public class HealthyBar : MonoBehaviour
                 RedBarRoutine = StartCoroutine(SlowBarFunctionDMG(redBarHP));
                 bActorLastHP = battleActor.hp;
 
+            }
+            else if (battleActor.hp > bActorLastHP)
+            {
+                if (GreenBarRoutine != null)
+                {
+                    StopCoroutine(GreenBarRoutine);
+                }
+                if (BlueBarRoutine != null)
+                {
+                    StopCoroutine(BlueBarRoutine);
+                }
+                BlueBarRoutine = StartCoroutine(FastBarFunctionHeal(blueBarHP));
+                slowBarLock = true;
+                GreenBarRoutine = StartCoroutine(SlowBarFunctionHeal(greenBarHP));
+                bActorLastHP = battleActor.hp;
             }
         }
     }
